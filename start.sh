85325d99092a3811e47a88232e6e6e1cbf9756c2
shuidyyswater <107785952+shuidyyswater@users.noreply.github.com>
2022-10-24 23:58:38 -0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 PORT=80
-UUID=5aaed9b7-7fe3-47c3-bb52-db59859ce198
+UUID=ef25798e-50b8-4ada-a567-cecd934d1c18
 WebPage=https://bing.com
 CaddyConfig=https://raw.githubusercontent.com/bsefwe/Rail/main/etc/Caddyfile
 XRayConfig=https://raw.githubusercontent.com/bsefwe/Rail/main/etc/xray.json
